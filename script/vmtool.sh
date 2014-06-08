290e53f1de361e949d3eb1e56bad7abaec2b4448
narkisr <narkisr@gmail.com>
2014-06-08 03:34:58 +0300

    cleaning up

--- script/vmtool.sh
+++ script/vmtool.sh
@@ -1,23 +1,5 @@
 #!/bin/bash -eux
 
-if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
-    echo "==> Installing VMware Tools"
-    # Assuming the following packages are installed
-    # apt-get install -y linux-headers-$(uname -r) build-essential perl
-
-    cd /tmp
-    mkdir -p /mnt/cdrom
-    mount -o loop /home/vagrant/linux.iso /mnt/cdrom
-    tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-
-    /tmp/vmware-tools-distrib/vmware-install.pl -d
-
-    rm /home/vagrant/linux.iso
-    umount /mnt/cdrom
-    rmdir /mnt/cdrom
-    rm -rf /tmp/VMwareTools-*
-fi
-
 if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     echo "==> Installing VirtualBox guest additions"
     # Assuming the following packages are installed
@@ -31,7 +13,7 @@ if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     rm /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso
     rm /home/vagrant/.vbox_version
 
-    if [[ $VBOX_VERSION = "4.3.10" ]]; then
-        ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+    if [[ $VBOX_VERSION = "4.3.12" ]]; then
+        ln -s /opt/VBoxGuestAdditions-4.3.12/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
     fi
 fi
