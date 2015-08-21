ecb63957a875dc7d1c65c843bb37a1011d4bb0fb
ronen <narkisr@gmail.com>
2015-08-22 02:56:46 +0300

    always latest script

--- script/cmtool.sh
+++ script/cmtool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
 echo "==> Installing Puppet"
-wget https://gist.githubusercontent.com/narkisr/6097786/raw/4b62bb2e557de08608117e788d092dac5025ca6e/puppet-preqs.sh
+wget https://gist.githubusercontent.com/narkisr/6097786/raw/puppet-preqs.sh
 chmod +x puppet-preqs.sh
 /bin/bash puppet-preqs.sh
